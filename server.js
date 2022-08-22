1bc7f58ca045313ea95aa6b19d7ba2d3570db2c2
Jordan Hobson <hobsoncjordan@gmail.com>
2022-08-22 13:47:03 -0500

    Object

--- server.js
+++ server.js
@@ -7,4 +7,16 @@ app.get("/", (req, res) => {
     console.log("Bop")
     res.send("Yo!")
 })
+
+app.get("/starwars", (req, res) => {
+    res.send({
+        characterName: "Luke Skywalker",
+        characterAge: 1500,
+        lightsaber: "green",
+        Dad: "Darth Vader",
+        quip: "May the force be with you."
+    })
+})
+
+
 app.listen(3001)
